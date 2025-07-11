f49b05f2adcfb0a6b567de76614643e13911b36d
JoaquinMunoz0 <145179105+JoaquinMunoz0@users.noreply.github.com>
2025-07-10 23:34:31 -0400

    cambiada la pantalla home y aagregada pantalla de busqueda

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import '../services/marvel_services.dart';
-import '../utils/util.dart';
-import 'character_list_screen.dart';
-import 'character_detail.dart';  // Importa la nueva pantalla
-
+import 'character_detail.dart';
+import 'character_page.dart';
+// import 'profile_page.dart'; // <-- DESCOMENTAR PARA CUANDO HAGAS LA PANTALLA DE PERFIL
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,7 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> allCharacters = [];
   List<dynamic> filteredCharacters = [];
   bool isLoading = true;
-  final TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -27,69 +25,57 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> loadCharacters() async {
-  const int totalCharactersMarvel = 1562;
-  final prefs = await SharedPreferences.getInstance();
-  final int desiredCount = prefs.getInt('heroCount') ?? 30;
-  const int maxAttempts = 10;
-  const int limitPerCall = 100;
+    const int totalCharactersMarvel = 1562;
+    final prefs = await SharedPreferences.getInstance();
+    final int desiredCount = prefs.getInt('heroCount') ?? 30;
+    const int maxAttempts = 10;
+    const int limitPerCall = 100;
 
-  Set<String> nombreBasesVistos = {};
-  List<dynamic> personajesUnicos = [];
+    Set<String> nombreBasesVistos = {};
+    List<dynamic> personajesUnicos = [];
 
-  int attempts = 0;
-
-  setState(() {
-    isLoading = true;
-  });
-
-  while (personajesUnicos.length < desiredCount && attempts < maxAttempts) {
-    attempts++;
-    final offset = (DateTime.now().millisecondsSinceEpoch + attempts * 777) %
-        (totalCharactersMarvel - limitPerCall);
-
-    final url =
-        'https://gateway.marvel.com/v1/public/characters?limit=$limitPerCall&offset=$offset&ts=1752117884&apikey=40a835d209da33c1145163d7b5d39c76&hash=18a95e3d649ebb36a0fb101234f189dc';
-
-    try {
-      final marvelService = MarvelService(url);
-      final data = await marvelService.getMarvelData();
-      final json = jsonDecode(data);
-      final results = json['data']['results'];
-
-      for (var personaje in results) {
-        if (personajesUnicos.length >= desiredCount) break;
-
-        String nombre = personaje['name'];
-        String nombreBase = nombre.contains('(')
-            ? nombre.split('(')[0].trim()
-            : nombre.trim();
-
-        if (!nombreBasesVistos.contains(nombreBase)) {
-          nombreBasesVistos.add(nombreBase);
-          personajesUnicos.add(personaje);
-        }
-      }
-    } catch (e) {
-      print('Error fetching characters: $e');
-      break;
-    }
-  }
-
-  setState(() {
-    allCharacters = personajesUnicos;
-    filteredCharacters = personajesUnicos;
-    isLoading = false;
-  });
-}
-
-  void filterCharacters(String query) {
-    final filtered = allCharacters.where((char) {
-      final name = AppUtils.cleanCharacterName(char['name']).toLowerCase();
-      return name.contains(query.toLowerCase());
-    }).toList();
+    int attempts = 0;
 
     setState(() {
-      filteredCharacters = filtered;
+      isLoading = true;
+    });
+
+    while (personajesUnicos.length < desiredCount && attempts < maxAttempts) {
+      attempts++;
+      final offset = (DateTime.now().millisecondsSinceEpoch + attempts * 777) %
+          (totalCharactersMarvel - limitPerCall);
+
+      final url =
+          'https://gateway.marvel.com/v1/public/characters?limit=$limitPerCall&offset=$offset&ts=1752117884&apikey=40a835d209da33c1145163d7b5d39c76&hash=18a95e3d649ebb36a0fb101234f189dc';
+
+      try {
+        final marvelService = MarvelService(url);
+        final data = await marvelService.getMarvelData();
+        final json = jsonDecode(data);
+        final results = json['data']['results'];
+
+        for (var personaje in results) {
+          if (personajesUnicos.length >= desiredCount) break;
+
+          String nombre = personaje['name'];
+          String nombreBase = nombre.contains('(')
+              ? nombre.split('(')[0].trim()
+              : nombre.trim();
+
+          if (!nombreBasesVistos.contains(nombreBase)) {
+            nombreBasesVistos.add(nombreBase);
+            personajesUnicos.add(personaje);
+          }
+        }
+      } catch (e) {
+        break;
+      }
+    }
+
+    setState(() {
+      allCharacters = personajesUnicos;
+      filteredCharacters = personajesUnicos;
+      isLoading = false;
     });
   }
 
@@ -99,103 +85,93 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("Marvel Viewer"),
         actions: [
+          // BOTON PANTALLA DE PERFIL
+          /*
           IconButton(
-            icon: const Icon(Icons.list),
-            tooltip: "Ver como lista",
+            icon: const Icon(Icons.person),
+            tooltip: "Perfil de usuario",
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => CharacterListScreen(characters: allCharacters),
+                  builder: (context) => const ProfilePage(), // <-- TIENE QUE TENER ESTA CLASE JOPUTA
                 ),
               );
             },
           ),
+          */
         ],
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                    controller: searchController,
-                    onChanged: filterCharacters,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: 'Buscar personaje...',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      prefixIcon: const Icon(Icons.search, color: Colors.white),
-                      filled: true,
-                      fillColor: Colors.grey[850],
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: PageView.builder(
-                    itemCount: filteredCharacters.length,
-                    controller: PageController(viewportFraction: 0.85),
-                    itemBuilder: (context, index) {
-                      final character = filteredCharacters[index];
-                      final name = character['name'];
-                      final thumbnail = character['thumbnail'];
-                      final imageUrl =
-                          '${thumbnail['path']}/portrait_uncanny.${thumbnail['extension']}';
+          : PageView.builder(
+              itemCount: filteredCharacters.length,
+              controller: PageController(viewportFraction: 0.85),
+              itemBuilder: (context, index) {
+                final character = filteredCharacters[index];
+                final name = character['name'];
+                final thumbnail = character['thumbnail'];
+                final imageUrl =
+                    '${thumbnail['path']}/portrait_uncanny.${thumbnail['extension']}';
 
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CharacterDetailPage(
-                                characterId: character['id'],
-                                characterName: name,
-                              ),
-                            ),
-                          );
-                        },
-                        child: Card(
-                          margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          elevation: 6,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Image.network(
-                                  imageUrl,
-                                  height: 300,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              const SizedBox(height: 12),
-                              Text(
-                                name,
-                                style: const TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 8),
-                            ],
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CharacterDetailPage(
+                          characterId: character['id'],
+                          characterName: name,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 6,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.network(
+                            imageUrl,
+                            height: 300,
+                            fit: BoxFit.cover,
                           ),
                         ),
-                      );
-                    },
+                        const SizedBox(height: 12),
+                        Text(
+                          name,
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 8),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                );
+              },
             ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Buscar personaje",
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const CharacterPage(),
+            ),
+          );
+        },
+        child: const Icon(Icons.search),
+      ),
     );
   }
 }
